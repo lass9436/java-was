25c5955dc4d143f8bf9feb8798d130f07d5a5385
lass9436 <lass9436@gmail.com>
2024-07-04 10:33:03 +0900

    [refactor] WebServer 리팩토링
    MyServer 클래스의 이름을 WebServer 로 변경하고 테스트에 적합한 코드 작성을 위해서 static field 를 정리했습니다.
    포트 번호와 백로그와 스레드 풀 사이즈를 런타임에 인스턴스를 생성하면서 설정할 수 있도록 했습니다.

---
+++ src/main/java/codesquad/server/WebServer.java
@@ -0,0 +1,38 @@
+package codesquad.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import codesquad.runnable.MyRunnable;
+
+public class WebServer {
+
+	private final int PORT;
+	private final int BACKLOG;
+	private final int THREAD_POOL_SIZE;
+
+	private boolean running;
+
+	public WebServer(int port, int backlog, int threadPoolSize) throws IOException {
+		this.PORT = port;
+		this.BACKLOG = backlog;
+		this.THREAD_POOL_SIZE = threadPoolSize;
+	}
+
+	public void start() throws IOException {
+		running = true;
+		ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+		try(ServerSocket serverSocket = new ServerSocket(PORT, BACKLOG)){
+			while (running) {
+				threadPool.submit(new MyRunnable(serverSocket.accept()));
+			}
+		}
+		threadPool.shutdown();
+	}
+
+	public void stop() {
+		running = false;
+	}
+}
