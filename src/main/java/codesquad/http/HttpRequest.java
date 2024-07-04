f0a07ccb077cf8d4601e715dcc182d653e29ed02
lass9436 <lass9436@gmail.com>
2024-07-05 00:42:10 +0900

    [refactor] HttpRequest 를 recode 로 변경
    모든 필드가 private final 이고 getter 를 가지고 있으므로 간단하게 레코드로 작성했습니다.

--- src/main/java/codesquad/http/HttpRequest.java
+++ src/main/java/codesquad/http/HttpRequest.java
@@ -4,42 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
-public class HttpRequest {
-
-	private final String method;
-	private final String url;
-	private final String version;
-	private final Map<String, List<String>> headers;
-	private final Map<String, List<String>> parameters;
-
-	public HttpRequest(String method, String url, String version, Map<String, List<String>> headers,
-		Map<String, List<String>> parameters) {
-		this.method = method;
-		this.url = url;
-		this.version = version;
-		this.headers = headers;
-		this.parameters = parameters;
-	}
-
-	public String getMethod() {
-		return method;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public Map<String, List<String>> getHeaders() {
-		return headers;
-	}
-
-	public Map<String, List<String>> getParameters() {
-		return parameters;
-	}
+public record HttpRequest(String method, String url, String version, Map<String, List<String>> headers,
+						  Map<String, List<String>> parameters) {
 
 	@Override
 	public String toString() {
