0bbb2ed89c1366e97c16853498ef34fa3ae0bbf0
lass9436 <lass9436@gmail.com>
2024-07-05 11:24:18 +0900

    [feat] 자주 사용하는 스트링 상수 작성
    CRLF 나 SPACE 등 자주 사용하는 스트링 값들을 static final 로 작성하였습니다.

---
+++ src/main/java/codesquad/StringUtils/Constants.java
@@ -0,0 +1,11 @@
+package codesquad.StringUtils;
+
+public class Constants {
+	public static final String CRLF = "\r\n";
+	public static final String SPACE = " ";
+	public static final String TAB = "\t";
+	public static final String NEWLINE = "\n";
+	public static final String CARRIAGE_RETURN = "\r";
+	public static final String COLON_SPACE = ": ";
+	public static final String COMMA_SPACE = ", ";
+}
