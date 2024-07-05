4a0ea0b02f2eaeadfe4f3fbcfba4dae9602f4356
lass9436 <lass9436@gmail.com>
2024-07-05 17:09:48 +0900

    [refactor] HttpStaticHandler 경로 수정
    상대경로로 파일을 찾고 있었는데, jar 파일을 실행하는 위치에 따라서 경로가 바뀔 수 있어서 classPath 를 찾는 방식으로 변경하였습니다.

--- src/main/java/codesquad/http/handler/HttpStaticHandler.java
+++ src/main/java/codesquad/http/handler/HttpStaticHandler.java
@@ -1,8 +1,8 @@
 package codesquad.http.handler;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,7 +20,7 @@ import codesquad.http.status.HttpStatusException;
 public class HttpStaticHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(HttpHandler.class);
-	private static final String STATIC_ROOT_PATH = "src/main/resources/static/";
+	private static final String STATIC_ROOT_PATH = "static";
 
 	private static final Map<String, String> MIME_TYPES = Map.of(
 		"html", "text/html",
@@ -43,30 +43,35 @@ public class HttpStaticHandler {
 	public HttpResponse handle(HttpRequest httpRequest) {
 		String url = httpRequest.url();
 		String path = urlMapping.getOrDefault(url, url);
-		File file = new File(STATIC_ROOT_PATH + path);
+		String resourcePath = STATIC_ROOT_PATH + path;
 
-		if (!file.exists() || file.isDirectory()) {
-			throw new HttpStatusException(HttpStatus.NOT_FOUND, "File not found");
-		}
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
+			if (inputStream == null) {
+				throw new HttpStatusException(HttpStatus.NOT_FOUND, "File not found");
+			}
 
-		try (FileInputStream fileInputStream = new FileInputStream(file)) {
+			byte[] body = readAllBytes(inputStream);
 			String version = httpRequest.version();
-
-			String extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+			String extension = resourcePath.substring(resourcePath.lastIndexOf(".") + 1);
 			String mimeType = MIME_TYPES.getOrDefault(extension, "application/octet-stream");
 
 			Map<String, List<String>> headers = new HashMap<>();
 			headers.put("Content-Type", List.of(mimeType));
-			headers.put("Content-Length", List.of(String.valueOf(file.length())));
+			headers.put("Content-Length", List.of(String.valueOf(body.length)));
 
-			byte[] body = new byte[(int)file.length()];
-			int bytesRead = fileInputStream.read(body);
-			if (bytesRead != body.length) {
-				throw new HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 정상적으로 읽지 못했습니다.");
-			}
 			return new HttpResponse(version, HttpStatus.OK, headers, body);
 		} catch (IOException e) {
-			throw new HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 정상적으로 읽지 못했습니다.");
+			throw new HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to read the file");
 		}
+	}
+
+	private byte[] readAllBytes(InputStream inputStream) throws IOException {
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		byte[] data = new byte[1024];
+		int nRead;
+		while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+			buffer.write(data, 0, nRead);
+		}
+		return buffer.toByteArray();
 	}
 }
