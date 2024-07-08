0fce78aa168cd022e08bf3838b1170107c694837
lass9436 <lass9436@gmail.com>
2024-07-08 16:35:57 +0900

    [feat] UserRepository 작성
    User 엔티티를 저장할 수 있는 Repository 클래스를 작성하였습니다. 내부적으로 private static final ConcurrentMap 으로 저장소를 만들어 UserRepository 인스턴스가 여러개일지라도 하나의 Map 을 공유할 수 있도록 하고, 수정할 수 없도록 했습니다.
    
    CRUD public 메서드를 노출합니다.

---
+++ src/main/java/codesquad/model/UserRepository.java
@@ -0,0 +1,43 @@
+package codesquad.model;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import codesquad.http.status.HttpStatus;
+import codesquad.http.status.HttpStatusException;
+
+public class UserRepository {
+
+	private static final ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+
+	public User create(User user) {
+		if(users.containsKey(user.userId())){
+			throw new HttpStatusException(HttpStatus.BAD_REQUEST, "User with id " + user.userId() + " already exists");
+		}
+		users.put(user.userId(), user);
+		return user;
+	}
+
+	public User findById(String userId) {
+		if(!users.containsKey(userId)){
+			throw new HttpStatusException(HttpStatus.NOT_FOUND, "User with id " + userId + " does not exist");
+		}
+		return users.get(userId);
+	}
+
+	public User update(User user) {
+		if(!users.containsKey(user.userId())){
+			throw new HttpStatusException(HttpStatus.NOT_FOUND, "User with id " + user.userId() + " does not exist");
+		}
+		users.put(user.userId(), user);
+		return user;
+	}
+
+	public void delete(String userId) {
+		if(!users.containsKey(userId)){
+			throw new HttpStatusException(HttpStatus.NOT_FOUND, "User with id " + userId + " does not exist");
+		}
+		users.remove(userId);
+	}
+
+}
