29a035f99be3cd33dcbb2a17d53f17f4f8061718
lass9436 <lass9436@gmail.com>
2024-07-09 11:00:37 +0900

    [feat] HttpRequest body 추가
    HttpRequest 레코드에 body 필드를 추가하고 toString 메서드를 수정하였습니다.
    
    중복되는 부분을 private mapToString 메서드로 작성하였습니다.

--- src/main/java/codesquad/http/dto/HttpRequest.java
+++ src/main/java/codesquad/http/dto/HttpRequest.java
@@ -4,24 +4,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public record HttpRequest(String method, String path, String version, Map<String, List<String>> headers,
-						  Map<String, List<String>> parameters) {
+						  Map<String, List<String>> parameters, JsonNode body) {
 
 	@Override
 	public String toString() {
-		StringBuilder headersString = new StringBuilder();
-		for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
-			StringJoiner valuesJoiner = new StringJoiner(", ");
-			for (String value : entry.getValue()) {
-				valuesJoiner.add(value);
-			}
-			headersString.append(entry.getKey()).append(": ").append(valuesJoiner).append("\n");
-		}
+		String headersString = mapToString(headers);
+		String parametersString = mapToString(parameters);
+
 		return "HttpRequest{" +
 			"method='" + method + '\'' +
 			", path='" + path + '\'' +
 			", version='" + version + '\'' +
 			", headers=" + headersString +
+			", parameters=" + parametersString +
+			", body=" + body.toString() +
 			'}';
+	}
+
+	private String mapToString(Map<String, List<String>> map) {
+		StringBuilder result = new StringBuilder();
+		for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+			StringJoiner valuesJoiner = new StringJoiner(", ");
+			for (String value : entry.getValue()) {
+				valuesJoiner.add(value);
+			}
+			result.append(entry.getKey()).append(": ").append(valuesJoiner).append("\n");
+		}
+		return result.toString();
 	}
 }
