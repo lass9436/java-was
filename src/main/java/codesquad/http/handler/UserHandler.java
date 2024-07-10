146d637d62b00c1a10e2e9f11c0382b0bbe3f17c
lass9436 <lass9436@gmail.com>
2024-07-11 01:40:30 +0900

    [feat] 로그인 기능 구현
    로그인하는 HttpFunction 메서드를 작성하여 로그인 기능을 구현하였습니다.

--- src/main/java/codesquad/http/handler/UserHandler.java
+++ src/main/java/codesquad/http/handler/UserHandler.java
@@ -16,6 +16,7 @@ import codesquad.http.dto.HttpResponse;
 import codesquad.http.status.HttpStatus;
 import codesquad.model.User;
 import codesquad.model.UserRepository;
+import codesquad.session.SessionManager;
 
 @HttpHandler
 public class UserHandler {
@@ -38,5 +39,25 @@ public class UserHandler {
 		logger.info("Creating user: {}", user);
 		Map<String, List<String>> headers = Map.of("Location", List.of("/index.html"));
 		return new HttpResponse(HttpVersion.HTTP_1_1, HttpStatus.FOUND, headers, new byte[0]);
+	}
+
+	@HttpFunction(path = "/login", method = HttpMethod.POST, type = HttpHandleType.DYNAMIC)
+	public HttpResponse login(HttpRequest httpRequest) {
+		Map<String, List<String>> body = httpRequest.body();
+		String id = body.get("id").get(0);
+		String password = body.get("password").get(0);
+
+		User user = userRepository.findById(id);
+		if (user != null && user.password().equals(password)) {
+			String sessionId = SessionManager.putSession("user", user);
+			Map<String, List<String>> headers = Map.of(
+				"Location", List.of("/index.html"),
+				"Set-Cookie", List.of("SID=" + sessionId + "; Path=/")
+			);
+			return new HttpResponse(HttpVersion.HTTP_1_1, HttpStatus.FOUND, headers, new byte[0]);
+		} else {
+			Map<String, List<String>> headers = Map.of("Location", List.of("/user/login_failed.html"));
+			return new HttpResponse(HttpVersion.HTTP_1_1, HttpStatus.FOUND, headers, new byte[0]);
+		}
 	}
 }
