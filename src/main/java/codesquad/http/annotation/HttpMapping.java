d275a56dbd8de373356925abc3c88dae13a16651
lass9436 <lass9436@gmail.com>
2024-07-10 12:56:38 +0900

    [feat] HttpMapping 어노테이션을 추가하였습니다.
    핸들러 동적 url 매핑을 위하여 어노테이션을 만들었습니다.

---
+++ src/main/java/codesquad/http/annotation/HttpMapping.java
@@ -0,0 +1,17 @@
+package codesquad.http.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import codesquad.http.constants.HttpHandleType;
+import codesquad.http.constants.HttpMethod;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface HttpMapping {
+	String path();
+	HttpMethod method();
+	HttpHandleType type();
+}
