be6436f4823751565846cd5684e0846cfa8e73dc
lass9436 <lass9436@gmail.com>
2024-07-11 16:46:01 +0900

    [feat] index.html 에서 자동으로 로그인 여부 요청
     index.html 에서 자동으로 로그인 여부를 요청하여 html 요소를 변경하여 로그인 여부를 알 수 있게 하였습니다.

--- src/main/java/codesquad/model/User.java
+++ src/main/java/codesquad/model/User.java
@@ -5,9 +5,9 @@ public record User(String userId, String password, String name, String email) {
 	@Override
 	public String toString() {
 		return "{" +
-			"userId='" + userId + '\'' +
-			", name='" + name + '\'' +
-			", email='" + email + '\'' +
+			"\"userId\": \"" + userId + "\"," +
+			"\"name\": \"" + name + "\"," +
+			"\"email\": \"" + email + "\"" +
 			'}';
 	}
 }
