7f026ad09bb32c17bdb15f3dc23eaa9e842dc6f1
lass9436 <lass9436@gmail.com>
2024-07-14 23:03:29 +0900

    [refactor] WebWorker 수정
    Function 제네릭 변경과, 템플릿 엔진의 추가에 따라 전체적인 HTTP 요청 처리 로직의 변경사항을 적용하였습니다.
    
    Container 에서의 초기화도 수정하였습니다.

--- src/main/java/codesquad/container/Container.java
+++ src/main/java/codesquad/container/Container.java
@@ -3,6 +3,7 @@ package codesquad.container;
 import codesquad.http.mapper.HttpDynamicHandlerMapper;
 import codesquad.http.mapper.HttpStaticHandlerMapper;
 import codesquad.http.registry.HttpHandlerRegistry;
+import codesquad.http.render.RenderEngine;
 import codesquad.server.WebWorker;
 
 public class Container {
@@ -12,13 +13,15 @@ public class Container {
 	private final HttpStaticHandlerMapper httpStaticHandlerMapper;
 	private final HttpDynamicHandlerMapper httpDynamicHandlerMapper;
 	private final HttpHandlerRegistry httpHandlerRegistry;
+	private final RenderEngine renderEngine;
 	private final WebWorker webWorker;
 
 	private Container() {
 		this.httpHandlerRegistry = new HttpHandlerRegistry();
 		this.httpDynamicHandlerMapper = new HttpDynamicHandlerMapper(httpHandlerRegistry);
 		this.httpStaticHandlerMapper = new HttpStaticHandlerMapper();
-		this.webWorker = new WebWorker(httpDynamicHandlerMapper, httpStaticHandlerMapper);
+		this.renderEngine = new RenderEngine();
+		this.webWorker = new WebWorker(httpDynamicHandlerMapper, httpStaticHandlerMapper, renderEngine);
 	}
 
 	public static Container getInstance() {
@@ -36,7 +39,11 @@ public class Container {
 		return httpDynamicHandlerMapper;
 	}
 
-	public HttpHandlerRegistry getHttpHandlerRegistry() { return httpHandlerRegistry; }
+	public HttpHandlerRegistry getHttpHandlerRegistry() {
+		return httpHandlerRegistry;
+	}
 
-	public WebWorker getWebWorker() { return webWorker; }
+	public WebWorker getWebWorker() {
+		return webWorker;
+	}
 }
--- src/main/java/codesquad/server/WebWorker.java
+++ src/main/java/codesquad/server/WebWorker.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,8 @@ import codesquad.http.dto.HttpResponse;
 import codesquad.http.mapper.HttpDynamicHandlerMapper;
 import codesquad.http.mapper.HttpStaticHandlerMapper;
 import codesquad.http.parser.HttpRequestParser;
+import codesquad.http.render.RenderData;
+import codesquad.http.render.RenderEngine;
 import codesquad.http.status.HttpStatusException;
 import codesquad.session.SessionManager;
 
@@ -23,46 +26,68 @@ public class WebWorker {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebWorker.class);
 
+	public static final ThreadLocal<HttpRequest> HTTP_REQUEST_THREAD_LOCAL = new ThreadLocal<>();
+	public static final ThreadLocal<HttpResponse> HTTP_RESPONSE_THREAD_LOCAL = new ThreadLocal<>();
+
 	private final HttpDynamicHandlerMapper httpDynamicHandlerMapper;
 	private final HttpStaticHandlerMapper httpStaticHandlerMapper;
+	private final RenderEngine renderEngine;
 
-	public WebWorker(HttpDynamicHandlerMapper httpDynamicHandlerMapper, HttpStaticHandlerMapper httpStaticHandlerMapper) {
+	public WebWorker(HttpDynamicHandlerMapper httpDynamicHandlerMapper,
+		HttpStaticHandlerMapper httpStaticHandlerMapper,
+		RenderEngine renderEngine
+	) {
 		this.httpDynamicHandlerMapper = httpDynamicHandlerMapper;
 		this.httpStaticHandlerMapper = httpStaticHandlerMapper;
+		this.renderEngine = renderEngine;
+
 	}
 
 	public void process(Socket socket) {
 		try (OutputStream clientOutput = socket.getOutputStream();
 			 BufferedReader clientInputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 			// 클라이언트 연결 로그 출력
-			logger.info("Client connected");
-
+			//logger.info("Client connected");
+			HTTP_REQUEST_THREAD_LOCAL.set(new HttpRequest());
+			HTTP_RESPONSE_THREAD_LOCAL.set(new HttpResponse());
 			try {
 				// HTTP Request
-				HttpRequest httpRequest = HttpRequestParser.parse(clientInputReader);
-				logger.debug(httpRequest.path());
+				HTTP_REQUEST_THREAD_LOCAL.set(HttpRequestParser.parse(clientInputReader));
+				HttpRequest httpRequest = HTTP_REQUEST_THREAD_LOCAL.get();
+				logger.info("{} {}", httpRequest.getMethod(), httpRequest.getPath());
 
-				// HTTP Response
-				// Dynamic
-				HttpResponse httpResponse = httpDynamicHandlerMapper.handle(httpRequest);
-				// Static
-				if (httpResponse == null) {
-					httpResponse = httpStaticHandlerMapper.handle(httpRequest);
+				// find handler
+				Function<Void, RenderData> handler = httpDynamicHandlerMapper.findHandler();
+
+				// static handle
+				if (handler == null) {
+					HTTP_RESPONSE_THREAD_LOCAL.set(httpStaticHandlerMapper.handle());
+				} else {
+					// dynamic handle
+					RenderData renderData = handler.apply(null);
+					// render
+					if (renderData != null) {
+						renderEngine.render(renderData);
+					}
 				}
-				clientOutput.write(httpResponse.getBytes());
+				clientOutput.write(HTTP_RESPONSE_THREAD_LOCAL.get().getBytes());
 
 			} catch (HttpStatusException e) {
 				logger.error("HTTP 상태 코드 예외 발생: ", e);
-				HttpResponse errorResponse = new HttpResponse(HttpVersion.HTTP_1_1, e.getStatus(), Map.of(),
+				HttpResponse httpResponse = new HttpResponse(HttpVersion.HTTP_1_1, e.getStatus(), Map.of(),
 					e.getStatus().getReasonPhrase().getBytes());
-				clientOutput.write(errorResponse.getBytes());
+				clientOutput.write(httpResponse.getBytes());
 			}
 			// write flush
 			clientOutput.flush();
 		} catch (IOException e) {
 			logger.error("클라이언트 소켓의 예외 발생: ", e);
+		} catch (Exception e) {
+			logger.error("알 수 없는 에러 ", e);
 		} finally {
 			SessionManager.removeThreadLocalSID();
+			HTTP_REQUEST_THREAD_LOCAL.remove();
+			HTTP_RESPONSE_THREAD_LOCAL.remove();
 		}
 	}
 }
