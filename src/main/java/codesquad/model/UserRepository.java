062440609f3448f2465531384d08d0ad45e3adfb
lass9436 <lass9436@gmail.com>
2024-07-14 23:01:24 +0900

    [refactor] User record 에서 class 로 수정
    템플릿 엔진에서 get 메서드를 이용하여 프로퍼티를 가져오므로 향후 확장성과 통일성을 위해서 record 객체를 getter 를 가지는 class 로 변경하였습니다.
    
    해당 변경사항에 맞춰 UserRepository 도 수정하였습니다.

--- src/main/java/codesquad/model/UserRepository.java
+++ src/main/java/codesquad/model/UserRepository.java
@@ -5,38 +5,35 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-import codesquad.http.status.HttpStatus;
-import codesquad.http.status.HttpStatusException;
-
 public class UserRepository {
 
 	private static final ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
 
 	public User create(User user) {
-		if(users.containsKey(user.userId())){
-			throw new IllegalArgumentException("User with id " + user.userId() + " already exists");
+		if (users.containsKey(user.getUserId())) {
+			throw new IllegalArgumentException("User with id " + user.getUserId() + " already exists");
 		}
-		users.put(user.userId(), user);
+		users.put(user.getUserId(), user);
 		return user;
 	}
 
 	public User findById(String userId) {
-		if(!users.containsKey(userId)){
+		if (!users.containsKey(userId)) {
 			throw new IllegalArgumentException("User with id " + userId + " does not exist");
 		}
 		return users.get(userId);
 	}
 
 	public User update(User user) {
-		if(!users.containsKey(user.userId())){
-			throw new IllegalArgumentException("User with id " + user.userId() + " does not exist");
+		if (!users.containsKey(user.getUserId())) {
+			throw new IllegalArgumentException("User with id " + user.getUserId() + " does not exist");
 		}
-		users.put(user.userId(), user);
+		users.put(user.getUserId(), user);
 		return user;
 	}
 
 	public void delete(String userId) {
-		if(!users.containsKey(userId)){
+		if (!users.containsKey(userId)) {
 			throw new IllegalArgumentException("User with id " + userId + " does not exist");
 		}
 		users.remove(userId);
@@ -45,5 +42,4 @@ public class UserRepository {
 	public List<User> findAll() {
 		return new ArrayList<>(users.values());
 	}
-
 }
