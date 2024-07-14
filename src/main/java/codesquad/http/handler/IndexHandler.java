142bc7788adc26f1e1149f555d859019f0e3783d
lass9436 <lass9436@gmail.com>
2024-07-15 00:50:51 +0900

    [feat] GET /user/list 동적 핸들러 매핑
    유저 리스트 요청을 동적으로 매핑하여 사용자의 상황에 맞게 화면을 표시할 수 있도록 했습니다.

--- src/main/java/codesquad/http/handler/IndexHandler.java
+++ src/main/java/codesquad/http/handler/IndexHandler.java
@@ -1,9 +1,9 @@
 package codesquad.http.handler;
 
-import static codesquad.server.WebWorker.*;
 import static codesquad.session.SessionManager.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -13,19 +13,19 @@ import codesquad.http.annotation.HttpFunction;
 import codesquad.http.annotation.HttpHandler;
 import codesquad.http.constants.HttpHandleType;
 import codesquad.http.constants.HttpMethod;
-import codesquad.http.dto.HttpRequest;
 import codesquad.http.render.RenderData;
 import codesquad.model.User;
+import codesquad.model.UserRepository;
 
 @HttpHandler
 public class IndexHandler {
 
 	private final Logger logger = LoggerFactory.getLogger(IndexHandler.class);
 
+	private final UserRepository userRepository = new UserRepository();
+
 	@HttpFunction(path = "/", method = HttpMethod.GET, type = HttpHandleType.DYNAMIC)
 	public RenderData handleIndex() {
-		HttpRequest httpRequest = HTTP_REQUEST_THREAD_LOCAL.get();
-
 		// 세션에서 User 인스턴스 획득
 		User user = (User)getSession("user");
 		Map<String, Object> model = new HashMap<>();
@@ -33,6 +33,26 @@ public class IndexHandler {
 
 		// RenderData 객체를 생성하여 뷰 이름과 모델을 설정합니다.
 		RenderData renderData = new RenderData("/index");
+		renderData.getModel().putAll(model);
+
+		return renderData;
+	}
+
+	@HttpFunction(path = "/user/list", method = HttpMethod.GET, type = HttpHandleType.DYNAMIC)
+	public RenderData handleUserList() {
+		// 자기 자신 조회
+		User user = (User)getSession("user");
+
+		// 전체 유저 목록 조회
+		List<User> users = userRepository.findAll();
+
+		// 모델
+		Map<String, Object> model = new HashMap<>();
+		model.put("users", users);
+		model.put("user", user);
+
+		// 렌더 데이터
+		RenderData renderData = new RenderData("/user/list");
 		renderData.getModel().putAll(model);
 
 		return renderData;
