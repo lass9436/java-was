6a7945b4484732d8fb60ad21ec3893ccad823103
lass9436 <lass9436@gmail.com>
2024-07-16 00:59:36 +0900

    [schema] users 와 posts 스키마 파일을 작성하였습니다.
    H2 데이터베이스에서 사용할 DDL sql 을 작성하였습니다.
    Database 클래스에서 해당 파일을 읽어서 테이블을 등록하는 것을 기대합니다.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE posts (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       user_id INT NOT NULL,
+                       username VARCHAR(50) NOT NULL,
+                       title VARCHAR(255) NOT NULL,
+                       content TEXT NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       FOREIGN KEY (user_id) REFERENCES users(id)
+);
