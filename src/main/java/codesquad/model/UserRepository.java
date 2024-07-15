de0027d22f14c482310930355c27d1ffc0391f4b
lass9436 <lass9436@gmail.com>
2024-07-15 22:56:46 +0900

    [refactor] 레포지토리 분리
    본래 유저 레포지토리의 메서드 시그니처만 인터페이스로 분리하고 구현 내용을 레포지토리 구현체 클래스를 따로 만들었습니다.
    인터페이스에는 @Repository 어노테이션을 부착하고 구현체에는 @RepositoryImpl 어노테이션을 부착하였습니다.
    DependencyFactory 가 해당 어노테이션을 파악하여 의존성을 관리해줄 것을 기대합니다.

--- src/main/java/codesquad/model/UserRepository.java
+++ src/main/java/codesquad/model/UserRepository.java
@@ -1,45 +1,19 @@
 package codesquad.model;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
-public class UserRepository {
+import codesquad.annotation.Repository;
 
-	private static final ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+@Repository
+public interface UserRepository {
 
-	public User create(User user) {
-		if (users.containsKey(user.getUserId())) {
-			throw new IllegalArgumentException("User with id " + user.getUserId() + " already exists");
-		}
-		users.put(user.getUserId(), user);
-		return user;
-	}
+	User create(User user);
 
-	public User findById(String userId) {
-		if (!users.containsKey(userId)) {
-			throw new IllegalArgumentException("User with id " + userId + " does not exist");
-		}
-		return users.get(userId);
-	}
+	User findById(String userId);
 
-	public User update(User user) {
-		if (!users.containsKey(user.getUserId())) {
-			throw new IllegalArgumentException("User with id " + user.getUserId() + " does not exist");
-		}
-		users.put(user.getUserId(), user);
-		return user;
-	}
+	User update(User user);
 
-	public void delete(String userId) {
-		if (!users.containsKey(userId)) {
-			throw new IllegalArgumentException("User with id " + userId + " does not exist");
-		}
-		users.remove(userId);
-	}
+	void delete(String userId);
 
-	public List<User> findAll() {
-		return new ArrayList<>(users.values());
-	}
+	List<User> findAll();
 }
---
+++ src/main/java/codesquad/model/UserRepositoryImpl.java
@@ -0,0 +1,53 @@
+package codesquad.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import codesquad.annotation.RepositoryImpl;
+
+@RepositoryImpl
+public class UserRepositoryImpl implements UserRepository {
+
+	private static final ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+
+	@Override
+	public User create(User user) {
+		if (users.containsKey(user.getUserId())) {
+			throw new IllegalArgumentException("User with id " + user.getUserId() + " already exists");
+		}
+		users.put(user.getUserId(), user);
+		return user;
+	}
+
+	@Override
+	public User findById(String userId) {
+		if (!users.containsKey(userId)) {
+			throw new IllegalArgumentException("User with id " + userId + " does not exist");
+		}
+		return users.get(userId);
+	}
+
+	@Override
+	public User update(User user) {
+		if (!users.containsKey(user.getUserId())) {
+			throw new IllegalArgumentException("User with id " + user.getUserId() + " does not exist");
+		}
+		users.put(user.getUserId(), user);
+		return user;
+	}
+
+	@Override
+	public void delete(String userId) {
+		if (!users.containsKey(userId)) {
+			throw new IllegalArgumentException("User with id " + userId + " does not exist");
+		}
+		users.remove(userId);
+	}
+
+	@Override
+	public List<User> findAll() {
+		return new ArrayList<>(users.values());
+	}
+}
