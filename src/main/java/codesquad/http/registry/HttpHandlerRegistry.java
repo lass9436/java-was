7a7fd87e05e0de6b6a5df5cb4db9bab61093fb74
lass9436 <lass9436@gmail.com>
2024-07-15 22:53:30 +0900

    [refactor] HttpHandlerRegistry 수정
    DependencyFactory 와 ComponentScanner 를 의존하여 HttpHandlerRegistry 의 역할만 할 수 있도록 메서드를 수정하였습니다.
    DependencyFactory 에서 의존성이 부여된 핸들러 인스턴스를 받아서 메소드 단위로 등록합니다.

--- src/main/java/codesquad/http/registry/HttpHandlerRegistry.java
+++ src/main/java/codesquad/http/registry/HttpHandlerRegistry.java
@@ -1,24 +1,18 @@
 package codesquad.http.registry;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import codesquad.http.annotation.HttpFunction;
-import codesquad.http.annotation.HttpHandler;
+import codesquad.annotation.HttpFunction;
+import codesquad.annotation.HttpHandler;
+import codesquad.container.ComponentScanner;
+import codesquad.container.DependencyFactory;
 import codesquad.http.constants.HttpHandleType;
 import codesquad.http.dto.HttpEndPoint;
 import codesquad.http.render.RenderData;
@@ -29,10 +23,13 @@ public class HttpHandlerRegistry {
 
 	private final String packageName = "codesquad.http.handler";
 
+	private final ComponentScanner componentScanner = new ComponentScanner();
+	private final DependencyFactory dependencyFactory = new DependencyFactory();
+
 	public Map<HttpEndPoint, Function<Void, RenderData>> getHandlers(HttpHandleType type) {
 		Map<HttpEndPoint, Function<Void, RenderData>> handlers = new HashMap<>();
 		try {
-			Class<?>[] classes = getClasses(packageName);
+			Class<?>[] classes = componentScanner.getClasses(packageName);
 			Arrays.stream(classes)
 				.filter(handlerClass -> handlerClass.isAnnotationPresent(HttpHandler.class))
 				.forEach(handlerClass -> registerHandlerMethods(handlers, handlerClass, type));
@@ -45,7 +42,7 @@ public class HttpHandlerRegistry {
 	private void registerHandlerMethods(Map<HttpEndPoint, Function<Void, RenderData>> handlers,
 		Class<?> handlerClass, HttpHandleType type) {
 		try {
-			Object handlerInstance = handlerClass.getDeclaredConstructor().newInstance();
+			Object handlerInstance = dependencyFactory.createHandlerInstance(handlerClass);
 			Arrays.stream(handlerClass.getMethods())
 				.filter(method -> method.isAnnotationPresent(HttpFunction.class))
 				.forEach(method -> {
@@ -69,75 +66,5 @@ public class HttpHandlerRegistry {
 				throw new RuntimeException(e);
 			}
 		};
-	}
-
-	private Class<?>[] getClasses(String packageName) throws ClassNotFoundException, IOException {
-		String path = packageName.replace('.', '/');
-		ClassLoader classLoader = getClass().getClassLoader();
-		URL resource = classLoader.getResource(path);
-		if (resource == null) {
-			throw new ClassNotFoundException("리소스를 찾을 수 없습니다: " + path);
-		}
-
-		if (resource.getProtocol().equals("file")) {
-			return getClassesFromFileSystem(packageName, resource);
-		} else if (resource.getProtocol().equals("jar")) {
-			return getClassesFromJarFile(packageName, resource);
-		} else {
-			throw new ClassNotFoundException("지원되지 않는 프로토콜입니다: " + resource.getProtocol());
-		}
-	}
-
-	private Class<?>[] getClassesFromFileSystem(String packageName, URL resource) throws ClassNotFoundException {
-		logger.info("파일 시스템을 사용합니다");
-		File directory;
-		try {
-			directory = new File(resource.toURI());
-		} catch (URISyntaxException e) {
-			directory = new File(resource.getPath());
-		}
-
-		if (!directory.exists()) {
-			throw new ClassNotFoundException(
-				packageName + " (" + directory.getPath() + ") 는 유효한 패키지로 보이지 않습니다");
-		}
-
-		File[] files = directory.listFiles();
-		if (files == null) {
-			throw new ClassNotFoundException("패키지에 대한 파일 목록을 가져오지 못했습니다: " + packageName);
-		}
-
-		return Arrays.stream(files)
-			.filter(file -> file.getName().endsWith(".class"))
-			.map(file -> {
-				String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-				try {
-					return Class.forName(className);
-				} catch (ClassNotFoundException e) {
-					throw new RuntimeException(e);
-				}
-			})
-			.toArray(Class<?>[]::new);
-	}
-
-	private Class<?>[] getClassesFromJarFile(String packageName, URL resource) throws IOException {
-		logger.info("JAR 시스템을 사용합니다");
-		String jarPath = resource.getPath().substring(5, resource.getPath().indexOf("!"));
-		try (JarFile jarFile = new JarFile(jarPath)) {
-			Enumeration<JarEntry> entries = jarFile.entries();
-			return Collections.list(entries).stream()
-				.filter(entry -> entry.getName().startsWith(packageName.replace('.', '/')))
-				.filter(entry -> entry.getName().endsWith(".class"))
-				.filter(entry -> !entry.isDirectory())
-				.map(entry -> {
-					String className = entry.getName().replace('/', '.').substring(0, entry.getName().length() - 6);
-					try {
-						return Class.forName(className);
-					} catch (ClassNotFoundException e) {
-						throw new RuntimeException(e);
-					}
-				})
-				.toArray(Class<?>[]::new);
-		}
 	}
 }
