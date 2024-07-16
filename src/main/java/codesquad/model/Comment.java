5a91c885b798b7f989f6d4abb5d1e2a4e6f53a5f
lass9436 <lass9436@gmail.com>
2024-07-16 13:14:51 +0900

    [feat] Comment 엔티티 추가
    댓글 작성을 구현하기 위해 Comment 엔티티를 작성하였습니다.

---
+++ src/main/java/codesquad/model/Comment.java
@@ -0,0 +1,66 @@
+package codesquad.model;
+
+public class Comment {
+	private int id;
+	private int postId;
+	private String username;
+	private String content;
+	private String createdAt;
+
+	public Comment(int postId, String username, String content) {
+		this.postId = postId;
+		this.username = username;
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			"\"id\": \"" + id + "\"," +
+			"\"postId\": \"" + postId + "\"," +
+			"\"username\": \"" + username + "\"," +
+			"\"content\": \"" + content + "\"," +
+			"\"createdAt\": \"" + createdAt + "\"" +
+			'}';
+	}
+}
